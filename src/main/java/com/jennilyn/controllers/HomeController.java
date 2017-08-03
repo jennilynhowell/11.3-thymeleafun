a49938fcfcef76a6ee104840c59397d5aadabf1e
Jennilyn Howell <jennilynhowell@me.com>
2017-08-03 09:39:10 -0400

    inital db setup + a little html

---
+++ src/main/java/com/jennilyn/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.jennilyn.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/about")
+    public String about(){
+        return "about";
+    }
+}
